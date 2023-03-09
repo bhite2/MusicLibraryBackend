27aa4620f0c71c828907303eaabb8bba6faa60c1
Bryan Hite <bhite2@mail.usf.edu>
2023-03-09 11:45:23 -0500

    Created Song model.

--- musiclibrary_project/settings.py
+++ musiclibrary_project/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xo&s_xf(9!v1c&5mj11e(-_to-l1*=48e5&*t*4hyzw6u98)z2'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -74,15 +71,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'musiclibrary_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -125,3 +114,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from musiclibrary_project.local_setting import *
+except ImportError:
+    pass
