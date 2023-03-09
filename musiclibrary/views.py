c43b8e00d93a834706ec4915a863321157314e06
Bryan Hite <bhite2@mail.usf.edu>
2023-03-09 14:35:16 -0500

    Created urls.py file in app.

--- musiclibrary/views.py
+++ musiclibrary/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SongSerializer
+from .models import Song
 
-# Create your views here.
+@api_view(['GET'])
+def songs_list(request):
+
+    return Response('ok')
