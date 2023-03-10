97059e06a1afbc66121b565dd8e4945b93284239
Bryan Hite <bhite2@mail.usf.edu>
2023-03-10 13:12:14 -0500

    Created GET to request by ID + Updated urls.py.

--- musiclibrary/views.py
+++ musiclibrary/views.py
@@ -15,3 +15,9 @@ def songs_list(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+@api_view(['GET'])
+def song_detail(request, pk):
+    print(pk)
+
+    return Response(pk)
