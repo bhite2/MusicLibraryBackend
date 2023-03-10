e26d6de727e08e130fba7742f6cd333e27005008
Bryan Hite <bhite2@mail.usf.edu>
2023-03-10 13:18:27 -0500

    Created PUT and DELETE for API.

--- musiclibrary/views.py
+++ musiclibrary/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -18,6 +19,15 @@ def songs_list(request):
     
 @api_view(['GET'])
 def song_detail(request, pk):
-    print(pk)
-
-    return Response(pk)
+    song = get_object_or_404(Song, pk=pk)
+    if request.method == "GET":
+        serializer = SongSerializer(car)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = SongSerializer(song, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        song.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
